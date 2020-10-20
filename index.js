82b25e05cadc50bf34f3d88bf7d5c5debcfb54cf
Chloe Steffens <caforonda103@yahoo.com>
2020-10-20 06:31:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function saturdayFun(activity="roller-skate") {
   console.log(`This Saturday, I want to ${activity}!`)
 }
+
+function mondayWork(activity="go to the office") {
+  console.log(`This Monday, I will ${activity}.`)
+}
