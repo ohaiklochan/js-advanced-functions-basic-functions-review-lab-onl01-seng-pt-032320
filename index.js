f7c45d1682c2387d0144132dbc8a16428283ddea
Chloe Steffens <caforonda103@yahoo.com>
2020-10-20 06:37:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,18 @@ let wrapAdjective = function(style="*") {
 
 const Calculator = {
   add: function(a, b) {
-    
+    return a + b;
+  }
+  
+  subtract: function(a, b) {
+    return a - b;
+  }
+  
+  multiply: function(a, b) {
+    return a * b;
+  }
+  
+  divide: function(a, b) {
+    return a / b; 
   }
 }
